f0012082acbc89bf2f8867e2d4275310cb315fe3
Ken Wilcox <wilcoxk@gmail.com>
2015-07-28 20:44:47 -0600

    Initial non threaded version

---
+++ ConcurrentCollections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConcurrentCollections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var orders = new Queue<string>();
+			PlaceOrders(orders, "Mark");
+			PlaceOrders(orders, "Ramdevi");
+
+			foreach(string order in orders) 
+			{
+				Console.WriteLine("ORDER: " + order);
+			}
+		}
+
+		static void PlaceOrders(Queue<string> orders, string customerName)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Thread.Sleep(1);
+				var orderName = string.Format("{0} wants t-shirt {1}", customerName, i + 1);
+				orders.Enqueue(orderName);
+			}
+		}
+	}
+}
