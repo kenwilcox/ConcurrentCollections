6c5a371082a7a908df9a8752cb56138c72a9337f
Ken Wilcox <wilcoxk@gmail.com>
2015-08-03 21:52:39 -0600

    Added ConcurrentQueueDemo

---
+++ ConcurrentQueueDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConcurrentQueueDemo
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            var shirts = new Queue<string>();
+            shirts.Enqueue("Pluralsight");
+            shirts.Enqueue("WordPress");
+            shirts.Enqueue("Code School");
+
+            Console.WriteLine("After enqueuing, count = " + shirts.Count);
+
+            var item1 = shirts.Dequeue();
+            Console.WriteLine("\r\nRemoving " + item1);
+
+            var item2 = shirts.Peek();
+            Console.WriteLine("Peeking   " + item2);
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (var item in shirts)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count);
+        }
+    }
+}
