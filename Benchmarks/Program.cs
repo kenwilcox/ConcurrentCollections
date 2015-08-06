fc5b0d833438f21a738ca490bff2a026a9e42b66
Ken Wilcox <wilcoxk@gmail.com>
2015-08-05 21:10:40 -0600

    Added Simple Benchmark app

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictSize = 1000000;
+
+            Console.WriteLine("Dictionary, single threaded:");
+            var dict = new Dictionary<int, int>();
+            SingleThreadBenchmark.TimeDict(dict, dictSize);
+
+        }
+    }
+}
---
+++ Benchmarks/SingleThreadBenchmark.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    class SingleThreadBenchmark
+    {
+        static void PopulateDict(IDictionary<int, int> dict, int dictSize)
+        {
+            for (var i = 0; i < dictSize; i++)
+            {
+                dict.Add(i, 0);
+            }
+
+            for (var i = 0; i < dictSize; i++)
+            {
+                dict[i] += i;
+            }
+        }
+
+        static int GetTotalValue(IDictionary<int, int> dict)
+        {
+            var total = 0;
+            foreach (var item in dict)
+            {
+                total += dict[item.Value];
+            }
+            return total;
+        }
+
+        public static void TimeDict(IDictionary<int, int> dict, int dictSize)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            PopulateDict(dict, dictSize);
+            stopwatch.Stop();
+            Console.WriteLine("Time taken to build dictionary (ms):     {0}", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            var total = GetTotalValue(dict);
+            stopwatch.Stop();
+            Console.WriteLine("Time taken to enumerate dictionary (ms): {0}", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("Total is " + total);
+            if (total != dictSize)
+                Console.WriteLine("ERROR IN TOTAL!");
+        }
+    }
+}
