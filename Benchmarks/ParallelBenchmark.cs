d62331b3eed887e8813d32bb23289e3eb07bf5a6
Ken Wilcox <wilcoxk@gmail.com>
2015-08-05 21:28:01 -0600

    I dislike how Visual Studio forgets to add a new class to git

---
+++ Benchmarks/ParallelBenchmark.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    class ParallelBenchmark
+    {
+        static void PopulateDictParallel(ConcurrentDictionary<int, int> dict, int dictSize)
+        {
+            Parallel.For(0, dictSize, (i) => dict.TryAdd(i, 0));
+            Parallel.For(0, dictSize, (i) =>
+            {
+                bool done = dict.TryUpdate(i, 1, 0);
+                if (!done) throw new Exception("Error updating. Old value was " + dict[i]);
+            });
+        }
+
+        static int GetTotalValueParallel(ConcurrentDictionary<int, int> dict)
+        {
+            int expectedTotal = dict.Count;
+
+            int total = 0;
+            Parallel.ForEach(dict, keyValPair =>
+            {
+                Interlocked.Add(ref total, keyValPair.Value);
+            });
+            return total;
+        }
+
+        public static void TimeDictParallel(ConcurrentDictionary<int, int> dict, int dictSize)
+        {
+            var stopwatch = new Stopwatch();
+            
+            stopwatch.Start();
+            PopulateDictParallel(dict, dictSize);
+            stopwatch.Stop();
+            Console.WriteLine("Time taken to build dictionary (ms):     {0}", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Restart();
+            var total = GetTotalValueParallel(dict);
+            stopwatch.Stop();
+            Console.WriteLine("Time taken to enumerate dictionary (ms): {0}", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("Total is " + total);
+            if (total != dictSize)
+                Console.WriteLine("ERROR IN TOTAL!");
+        }
+    }
+}
