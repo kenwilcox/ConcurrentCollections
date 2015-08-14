6ef807f64913985c727ca1170fa02d60b288694d
Ken Wilcox <wilcoxk@gmail.com>
2015-08-14 10:50:26 -0600

    Modifying an collection while enumerating causing an exception

---
+++ EnumerateDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumerateDictionary
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            var stock = new Dictionary<string, int>
+            {
+                {"jDays", 0},
+                { "Code School", 0},
+                { "Buddhist Geeks", 0}
+            };
+
+            foreach (var shirt in stock)
+            {
+                stock["jDays"] += 1;
+                Console.WriteLine(shirt.Key + ": " + shirt.Value);
+            }
+        }
+    }
+}
